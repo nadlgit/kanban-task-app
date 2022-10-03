28db2a9623a4eb6398f6e514697ecb06d6162ad5
Nadine <nadlgit@users.noreply.github.com>
2022-10-03 12:36:53 -0400

    feat: modified board repository interface

--- src/core/ports/board/board-repository.ts
+++ src/core/ports/board/board-repository.ts
@@ -1,40 +1,53 @@
 import type { BoardEntity, ColumnEntity, TaskEntity, UniqueId } from 'core/entities';
 
 type BoardList = Pick<BoardEntity, 'id' | 'name'>[];
-type Position = { index: number };
 type BoardInfo = Omit<BoardEntity, 'id' | 'columns'>;
-type ColumnInfo = Omit<ColumnEntity, 'id' | 'tasks'> & Position;
-type TaskInfo = Omit<TaskEntity, 'id'> & Position;
+type ColumnInfo = Omit<ColumnEntity, 'id' | 'tasks'>;
+type TaskInfo = Omit<TaskEntity, 'id'>;
 
 export type BoardRepository = {
   getBoardList: (userId: UniqueId) => Promise<BoardList>;
   getBoard: (userId: UniqueId, boardId: UniqueId) => Promise<BoardEntity>;
 
+  listenToBoardListChange: (userId: UniqueId, callback: () => void) => () => void;
+  listenToBoardChange: (userId: UniqueId, boardId: UniqueId, callback: () => void) => () => void;
+
   addBoard: (
     userId: UniqueId,
-    board: BoardInfo & { columns: ColumnInfo[] }
-  ) => Promise<BoardEntity>;
-  addColumn: (userId: UniqueId, boardId: UniqueId, column: ColumnInfo) => Promise<ColumnEntity>;
+    board: BoardInfo & { columns: ColumnInfo[] },
+    index?: number
+  ) => Promise<void>;
+  addColumn: (
+    userId: UniqueId,
+    boardId: UniqueId,
+    column: ColumnInfo,
+    index?: number
+  ) => Promise<void>;
   addTask: (
     userId: UniqueId,
     boardId: UniqueId,
     columnId: UniqueId,
-    task: TaskInfo
-  ) => Promise<TaskEntity>;
+    task: TaskInfo,
+    index?: number
+  ) => Promise<void>;
 
-  updateBoard: (userId: UniqueId, boardId: UniqueId, board: BoardInfo) => Promise<void>;
+  updateBoard: (
+    userId: UniqueId,
+    board: { id: UniqueId } & BoardInfo,
+    index?: number
+  ) => Promise<void>;
   updateColumn: (
     userId: UniqueId,
     boardId: UniqueId,
-    columnId: UniqueId,
-    column: ColumnInfo
+    column: { id: UniqueId } & ColumnInfo,
+    index?: number
   ) => Promise<void>;
   updateTask: (
     userId: UniqueId,
     boardId: UniqueId,
     columnId: UniqueId,
-    taskId: UniqueId,
-    task: TaskInfo,
+    task: { id: UniqueId } & TaskInfo,
+    index?: number,
     oldColumnId?: UniqueId
   ) => Promise<void>;
 
