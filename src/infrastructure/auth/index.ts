ec9880c622216f2c6adabc8c33f26beb3f8cb14d
Nadine <nadlgit@users.noreply.github.com>
2022-10-10 11:01:00 -0400

    feat: removed fake auth repository

--- src/infrastructure/auth/index.ts
+++ src/infrastructure/auth/index.ts
@@ -1,7 +1,3 @@
-// import { FakeAuthRepository } from './fake-auth-repository';
-
-// export const authRepository = new FakeAuthRepository();
-
 import { FirebaseAuthRepository } from './firebase-auth-repository';
 
 export const authRepository = new FirebaseAuthRepository();
