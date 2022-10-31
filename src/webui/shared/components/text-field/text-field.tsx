db6e29688cd738d16a7c793d503a7d94fc3d865d
Nadine <nadlgit@users.noreply.github.com>
2022-10-31 18:08:58 -0400

    refactor: better usage of forwardRef and made label props optional

--- src/webui/shared/components/text-field/text-field.tsx
+++ src/webui/shared/components/text-field/text-field.tsx
@@ -3,8 +3,8 @@ import styles from './text-field.module.css';
 import { forwardRef } from 'react';
 import type { DetailedHTMLProps, InputHTMLAttributes } from 'react';
 
-type TextFieldProps = {
-  label: string;
+export type TextFieldProps = {
+  label?: string;
   error?: string;
   fullWidth?: boolean;
 } & Required<
@@ -12,31 +12,32 @@ type TextFieldProps = {
 > &
   Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'name'>;
 
-export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ label, error, fullWidth = true, name, id, type, className, ...otherprops }, ref) => {
-    const htmlId = id ?? name;
-    const cssClass = `${fullWidth ? styles['full-width'] : ''} ${error ? styles.invalid : ''} ${
-      className ?? ''
-    }`;
-    const otherInputProps = ref ? { ref, ...otherprops } : { ...otherprops };
-    return (
-      <p className={cssClass}>
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(function TextField(
+  { label, error, fullWidth = true, name, id, type, className, ...otherProps },
+  ref
+) {
+  const htmlId = id ?? name;
+  const cssClass = `${fullWidth ? styles['full-width'] : ''} ${error ? styles.invalid : ''} ${
+    className ?? ''
+  }`;
+  return (
+    <p className={cssClass}>
+      {label && (
         <label htmlFor={htmlId} className={styles.label}>
           {label}
         </label>
-        <input
-          id={htmlId}
-          name={name}
-          type={type ?? 'text'}
-          className={styles.input}
-          {...otherInputProps}
-        />
-        <span role="status" className={styles.errormsg}>
-          {error}
-        </span>
-      </p>
-    );
-  }
-);
-
-TextField.displayName = 'TextField';
+      )}
+      <input
+        ref={ref}
+        id={htmlId}
+        name={name}
+        type={type ?? 'text'}
+        className={styles.input}
+        {...otherProps}
+      />
+      <span role="status" className={styles.errormsg}>
+        {error}
+      </span>
+    </p>
+  );
+});
