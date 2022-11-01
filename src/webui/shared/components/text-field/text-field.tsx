380ac75c68295c1674a2dc8a19366ae1a9e0d97a
Nadine <nadlgit@users.noreply.github.com>
2022-11-01 16:55:40 -0400

    feat: modified TextInput component props and style

--- src/webui/shared/components/text-field/text-field.tsx
+++ src/webui/shared/components/text-field/text-field.tsx
@@ -7,18 +7,19 @@ export type TextFieldProps = {
   label?: string;
   error?: string;
   fullWidth?: boolean;
+  onDelete?: () => void;
 } & Required<
   Pick<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'name'>
 > &
   Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'name'>;
 
 export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(function TextField(
-  { label, error, fullWidth = true, name, id, type, className, ...otherProps },
+  { label, error, fullWidth = true, onDelete, name, id, type, className, ...otherProps },
   ref
 ) {
   const htmlId = id ?? name;
 
-  const cssClasses = [] as string[];
+  const cssClasses = [styles.base];
   fullWidth && cssClasses.push(styles['full-width']);
   error && cssClasses.push(styles.invalid);
   className && cssClasses.push(className);
@@ -30,17 +31,34 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(function T
           {label}
         </label>
       )}
-      <input
-        ref={ref}
-        id={htmlId}
-        name={name}
-        type={type ?? 'text'}
-        className={styles.input}
-        {...otherProps}
-      />
-      <span role="status" className={styles.errormsg}>
-        {error}
+      <span className={styles.inputcontainer}>
+        <input
+          ref={ref}
+          id={htmlId}
+          name={name}
+          type={type ?? 'text'}
+          className={styles.input}
+          {...otherProps}
+        />
+        {onDelete && (
+          <button type="button" onClick={onDelete} className={styles.delete}>
+            <SVGIconCross />
+          </button>
+        )}
+        <span role="status" className={styles.errormsg}>
+          {error}
+        </span>
       </span>
     </p>
   );
 });
+
+const SVGIconCross = () => (
+  <svg role="img" width="15" height="15" xmlns="http://www.w3.org/2000/svg">
+    <title>Delete item</title>
+    <g fill="currentColor" fillRule="evenodd">
+      <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
+      <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
+    </g>
+  </svg>
+);
