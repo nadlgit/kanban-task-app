e593c7d86fe9f9702847d47523a478b880805aae
Nadine <nadlgit@users.noreply.github.com>
2022-11-02 15:32:53 -0400

    refactor: merged board list and active id states

--- src/webui/board/store/context-provider.tsx
+++ src/webui/board/store/context-provider.tsx
@@ -1,27 +1,27 @@
 import { useCallback, useEffect, useReducer, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 
-import { activeBoardIdReducer } from './active-id-reducer';
+import { boardListReducer } from './board-list-reducer';
 import { BoardContext } from './context';
-import type { BoardList, UniqueId } from 'core/entities';
+import type { UniqueId } from 'core/entities';
 import { getBoardList, onBoardListChange } from 'core/usecases';
 
 type BoardContextProviderProps = PropsWithChildren;
 
 export const BoardContextProvider = ({ children }: BoardContextProviderProps) => {
   const [loading, setLoading] = useState(true);
-  const [boardList, setBoardList] = useState<BoardList>([]);
-  const [activeBoardId, dispatchActiveBoardId] = useReducer(activeBoardIdReducer, null);
+  const [{ boardList, activeBoardId }, dispatchBoardList] = useReducer(boardListReducer, {
+    boardList: [],
+    activeBoardId: null,
+  });
   const setActiveBoardId = useCallback((id: UniqueId | null) => {
-    dispatchActiveBoardId({ type: 'SET', payload: id });
+    dispatchBoardList({ type: 'SET_ACTIVE_ID', id });
   }, []);
 
   useEffect(() => {
     const updateBoardList = async () => {
       setLoading(true);
-      const list = await getBoardList();
-      setBoardList(list);
-      dispatchActiveBoardId({ type: 'RESET', payload: list });
+      dispatchBoardList({ type: 'SET_LIST', list: await getBoardList() });
       setLoading(false);
     };
     updateBoardList();
