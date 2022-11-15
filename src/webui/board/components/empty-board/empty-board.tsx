e86f1b79b67bad8b386e47a2a80ee6c07351a151
Nadine <nadlgit@users.noreply.github.com>
2022-11-15 08:50:41 -0400

    feat: updated CSS

--- src/webui/board/components/empty-board/empty-board.tsx
+++ src/webui/board/components/empty-board/empty-board.tsx
@@ -1,4 +1,4 @@
-import styles from 'empty-board.module.css';
+import styles from './empty-board.module.css';
 
 import { Button } from 'webui/shared';
 
@@ -6,9 +6,9 @@ type EmptyBoardProps = { addNewColumn: () => void };
 
 export const EmptyBoard = ({ addNewColumn }: EmptyBoardProps) => {
   return (
-    <div>
-      <p>This board is empty. Create a new column to get started.</p>
-      <Button variant="primary-s" fullWidth={false} onClick={addNewColumn}>
+    <div className={styles.container}>
+      <p className={styles.msg}>This board is empty. Create a new column to get started.</p>
+      <Button variant="primary-l" fullWidth={false} onClick={addNewColumn}>
         + Add New Column
       </Button>
     </div>
