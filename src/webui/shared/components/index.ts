e9376d6e024a9f8eab078ff28ca7b88b649a481a
Nadine <nadlgit@users.noreply.github.com>
2022-11-15 16:00:17 -0400

    feat: added TextArea component

--- src/webui/shared/components/index.ts
+++ src/webui/shared/components/index.ts
@@ -3,5 +3,6 @@ export * from './icon';
 export * from './logo';
 export * from './menu';
 export * from './modal';
+export * from './text-area';
 export * from './text-field';
 export * from './text-field-group';
---
+++ src/webui/shared/components/text-area/text-area.tsx
@@ -0,0 +1,42 @@
+import styles from './text-area.module.css';
+
+import { forwardRef } from 'react';
+import type { DetailedHTMLProps, TextareaHTMLAttributes } from 'react';
+
+export type TextAreaProps = {
+  label: string;
+  error?: string;
+} & Required<
+  Pick<DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'name'>
+> &
+  Omit<DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'name'>;
+
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(function TextArea(
+  { label, error, name, id, className, rows, ...otherProps },
+  ref
+) {
+  const htmlId = id ?? name;
+
+  const cssClasses = [styles.base];
+  error && cssClasses.push(styles.invalid);
+  className && cssClasses.push(className);
+
+  return (
+    <p className={cssClasses.join(' ')}>
+      <label htmlFor={htmlId} className={styles.label}>
+        {label}
+      </label>
+      <textarea
+        ref={ref}
+        id={htmlId}
+        name={name}
+        className={styles.input}
+        rows={rows ?? 4}
+        {...otherProps}
+      />
+      <span role="status" className={styles.errormsg}>
+        {error}
+      </span>
+    </p>
+  );
+});
