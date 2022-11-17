68c75e90d40e1975d92e72e52929a94ae06f31cd
Nadine <nadlgit@users.noreply.github.com>
2022-11-17 16:49:14 -0400

    feat: updated ModalHeading with menu option

--- src/webui/shared/components/modal/modal-heading.tsx
+++ src/webui/shared/components/modal/modal-heading.tsx
@@ -1,12 +1,20 @@
 import styles from './modal-heading.module.css';
 
 import { DialogHeading } from 'ariakit';
-import type { PropsWithChildren } from 'react';
+import type { PropsWithChildren, ReactNode } from 'react';
 
-type ModalHeadingProps = PropsWithChildren<{ variant?: 'destructive' }>;
+type ModalHeadingProps = PropsWithChildren<{
+  variant?: 'destructive';
+  menu?: ReactNode;
+}>;
 
-export const ModalHeading = ({ variant, children }: ModalHeadingProps) => {
+export const ModalHeading = ({ variant, menu, children }: ModalHeadingProps) => {
   const cssClasses = [styles.heading];
   variant && cssClasses.push(styles[variant]);
-  return <DialogHeading className={cssClasses.join(' ')}>{children}</DialogHeading>;
+  return (
+    <DialogHeading className={cssClasses.join(' ')}>
+      <span>{children}</span>
+      {menu && <span>{menu}</span>}
+    </DialogHeading>
+  );
 };
