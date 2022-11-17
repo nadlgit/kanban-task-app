2ef22e4992dd983a37370e7ad8bb07154f1ccd58
Nadine <nadlgit@users.noreply.github.com>
2022-11-17 16:50:13 -0400

    feat: updated ViewTask component

--- src/webui/board/components/view-task/task-content.tsx
+++ src/webui/board/components/view-task/task-content.tsx
@@ -1,9 +1,47 @@
-export const TaskContent = () => {
+import styles from './task-content.module.css';
+
+import type { ComponentProps } from 'react';
+
+import type { TaskEntity, UniqueId } from 'core/entities';
+import { Checkbox, Dropdown } from 'webui/shared';
+
+type TaskContentProps = {
+  task: Pick<TaskEntity, 'description' | 'subtasks'> & { statusId: UniqueId };
+  statusList: ComponentProps<typeof Dropdown>['items'];
+  onTaskStatusChange: (statusId: UniqueId) => void;
+  onSubtaskStatusChange: (subtaskIndex: number, status: boolean) => void;
+};
+
+export const TaskContent = ({
+  task,
+  statusList,
+  onTaskStatusChange,
+  onSubtaskStatusChange,
+}: TaskContentProps) => {
   return (
-    <div>
-      <p>Description</p>
-      <p>Subtasks</p>
-      <p>Status</p>
-    </div>
+    <>
+      <p className={styles.description}>{task.description}</p>
+      <div>
+        <p className={styles.label}>
+          {`Subtasks (${
+            task.subtasks.filter(({ isCompleted }) => isCompleted === true).length
+          } of ${task.subtasks.length})`}
+        </p>
+        {task.subtasks.map(({ title, isCompleted }, idx) => (
+          <Checkbox
+            key={idx}
+            label={title}
+            defaultChecked={isCompleted}
+            onChange={(e) => onSubtaskStatusChange(idx, e.target.checked)}
+          />
+        ))}
+      </div>
+      <Dropdown
+        label="Current Status"
+        items={statusList}
+        defaultValue={task.statusId}
+        onChange={onTaskStatusChange}
+      />
+    </>
   );
 };
--- src/webui/board/components/view-task/view-task.tsx
+++ src/webui/board/components/view-task/view-task.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { TaskContent } from './task-content';
 import { TaskMenu } from './task-menu';
@@ -14,23 +14,78 @@ type ViewTaskProps = {
 };
 
 export const ViewTask = ({ isOpen, close, board, columnId, taskId }: ViewTaskProps) => {
-  const [hide, setHide] = useState(false);
-  const task = board.columns
-    .find(({ id }) => id === columnId)
-    ?.tasks.find(({ id }) => id === taskId);
+  const task = {
+    title: '',
+    description: '',
+    subtasks: [],
+    ...board.columns.find(({ id }) => id === columnId)?.tasks.find(({ id }) => id === taskId),
+    statusId: columnId,
+  };
+  const statusList = board.columns.map(({ id, name }) => ({ value: id, label: name }));
+
+  const [taskStatus, setTaskStatus] = useState(task.statusId);
+  const [subtasksStatus, setSubtasksStatus] = useState(
+    task.subtasks.map(({ isCompleted }) => isCompleted)
+  );
+  const [hideModal, setHideModal] = useState(false);
+  const [closeModal, setCloseModal] = useState(false);
+
+  const setSubtaskStatus = (index: number, status: boolean) => {
+    setSubtasksStatus((l) => {
+      const list = [...l];
+      list[index] = status;
+      return list;
+    });
+  };
+
+  const hideView = () => setHideModal(true);
+  const closeView = () => setCloseModal(true);
+
+  useEffect(() => {
+    if (closeModal) {
+      console.log(
+        '%c should update task status',
+        'background-color:lime;',
+        taskStatus !== columnId
+      );
+      console.log(
+        '%c should update subtasks status',
+        'background-color:lime;',
+        !subtasksStatus.every((status, idx) => status === task.subtasks[idx].isCompleted)
+      );
+      //todo: persist changes
+      close();
+    }
+  }, [close, closeModal, columnId, subtasksStatus, task.subtasks, taskStatus]);
+
   return (
-    <Modal isOpen={isOpen} onClose={close} hide={hide}>
-      <ModalHeading>
-        <span> {task?.title}</span>
-        <TaskMenu
-          hideView={() => setHide(true)}
-          closeView={close}
-          board={board}
-          columnId={columnId}
-          taskId={taskId}
-        />
+    <Modal isOpen={isOpen} onClose={closeView} hide={hideModal}>
+      <ModalHeading
+        menu={
+          <TaskMenu
+            hideView={hideView}
+            closeView={closeView}
+            board={board}
+            columnId={columnId}
+            taskId={taskId}
+          />
+        }
+      >
+        {task.title}
       </ModalHeading>
-      <TaskContent />
+      <TaskContent
+        task={{
+          description: task.description,
+          subtasks: task.subtasks.map(({ title }, idx) => ({
+            title,
+            isCompleted: subtasksStatus[idx],
+          })),
+          statusId: task.statusId,
+        }}
+        statusList={statusList}
+        onTaskStatusChange={setTaskStatus}
+        onSubtaskStatusChange={setSubtaskStatus}
+      />
     </Modal>
   );
 };
