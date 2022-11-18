21120cd3ea26fecfbb3454b9b3d9f9bbadcb4799
Nadine <nadlgit@users.noreply.github.com>
2022-11-17 21:39:45 -0400

    feat: updated EditTask component

---
+++ src/webui/board/components/edit-task/edit-task-form.tsx
@@ -0,0 +1,120 @@
+import { useEffect, useMemo } from 'react';
+import type { ComponentProps, FormEventHandler } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+import type { TaskEntity, UniqueId } from 'core/entities';
+import { generateId } from 'infrastructure/utils';
+import {
+  boardTextAreaRegisterOptions,
+  boardTextInputRegisterOptions,
+  Button,
+  Dropdown,
+  TextArea,
+  TextField,
+  TextFieldGroup,
+  updateTextInputErrors,
+  useTextFieldGroupInputList,
+} from 'webui/shared';
+
+type EditTaskFormProps = {
+  task: Omit<TaskEntity, 'id'> & { statusId: UniqueId };
+  statusList: ComponentProps<typeof Dropdown>['items'];
+  onSubmit: () => void;
+};
+
+export const EditTaskForm = ({ task, statusList, onSubmit }: EditTaskFormProps) => {
+  const { register, handleSubmit, formState, control } = useForm({ shouldUnregister: true });
+
+  const subtaskItemLabel = 'Subtask Title';
+  const newSubtaskItemName = () => generateId('newsubtask');
+  const newSubtaskPlaceholder = 'e.g. Make coffee';
+  const existingSubtaskItemName = (index: number) => `subtask-${index}`;
+
+  const initialSubtasks = useMemo(
+    () =>
+      task.subtasks.map(({ title }, idx) => ({
+        ...register(existingSubtaskItemName(idx), boardTextInputRegisterOptions),
+        label: subtaskItemLabel,
+        defaultValue: title,
+        placeholder: newSubtaskPlaceholder,
+      })),
+    [register, task.subtasks]
+  );
+
+  const {
+    list: subtasks,
+    addItem: addSubtaskItem,
+    deleteItem: deleteSubtaskItem,
+    setError: setSubtaskItemError,
+  } = useTextFieldGroupInputList(initialSubtasks);
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    handleSubmit((data) => {
+      onSubmit();
+      console.log('%c data', 'background-color:lime;', data);
+      //todo
+    })(e);
+  };
+
+  useEffect(() => {
+    updateTextInputErrors(
+      formState,
+      subtasks.map(({ name, error }) => ({ name, error })),
+      setSubtaskItemError
+    );
+  }, [formState, subtasks, setSubtaskItemError]);
+
+  return (
+    <form onSubmit={handleFormSubmit} noValidate>
+      <TextField
+        {...register('tasktitle', boardTextInputRegisterOptions)}
+        label="Title"
+        defaultValue={task.title}
+        placeholder="e.g. Take coffee break"
+        error={formState.errors['tasktitle']?.message as string | undefined}
+      />
+      <TextArea
+        {...register('taskdesc', boardTextAreaRegisterOptions)}
+        label="Description"
+        defaultValue={task.description}
+        placeholder="e.g. It’s always good to take a break. This 15 minute break will recharge the batteries a little."
+        error={formState.errors['taskdesc']?.message as string | undefined}
+      />
+      <TextFieldGroup
+        groupLabel="Subtasks"
+        inputList={subtasks}
+        addLabel="+ Add New Subtask"
+        onAdd={() => {
+          addSubtaskItem({
+            ...register(newSubtaskItemName(), boardTextInputRegisterOptions),
+            label: subtaskItemLabel,
+            placeholder: newSubtaskPlaceholder,
+          });
+        }}
+        onDelete={(inputName: string) => {
+          deleteSubtaskItem(inputName);
+        }}
+      />
+      <Controller
+        name="taskstatus"
+        control={control}
+        defaultValue={task.statusId}
+        render={({ field }) => (
+          <Dropdown
+            label="Status"
+            items={statusList}
+            name={field.name}
+            onChange={field.onChange}
+            defaultValue={task.statusId}
+          />
+        )}
+        shouldUnregister={true}
+      />
+      <Button variant="primary-s" type="submit">
+        Save Changes
+      </Button>
+    </form>
+  );
+};
--- src/webui/board/components/edit-task/edit-task.tsx
+++ src/webui/board/components/edit-task/edit-task.tsx
@@ -1,3 +1,6 @@
+import type { ComponentProps } from 'react';
+
+import { EditTaskForm } from './edit-task-form';
 import type { BoardEntity, UniqueId } from 'core/entities';
 import { Modal, ModalHeading } from 'webui/shared';
 
@@ -10,13 +13,32 @@ type EditTaskProps = {
 };
 
 export const EditTask = ({ isOpen, close, board, columnId, taskId }: EditTaskProps) => {
-  const task = board.columns
-    .find(({ id }) => id === columnId)
-    ?.tasks.find(({ id }) => id === taskId);
+  const task = {
+    title: '',
+    description: '',
+    subtasks: [],
+    ...board.columns.find(({ id }) => id === columnId)?.tasks.find(({ id }) => id === taskId),
+    statusId: columnId,
+  };
+
+  const onSubmit: ComponentProps<typeof EditTaskForm>['onSubmit'] = async () => {
+    //todo
+    close();
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={close}>
       <ModalHeading>Edit Task</ModalHeading>
+      <EditTaskForm
+        task={{
+          title: task.title,
+          description: task.description,
+          subtasks: task.subtasks,
+          statusId: task.statusId,
+        }}
+        statusList={board.columns.map(({ id, name }) => ({ value: id, label: name }))}
+        onSubmit={onSubmit}
+      />
     </Modal>
   );
 };
