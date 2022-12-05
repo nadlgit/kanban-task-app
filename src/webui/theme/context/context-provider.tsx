512b6278e32a4be656d27dabc11dba533c18f217
Nadine <nadlgit@users.noreply.github.com>
2022-12-04 21:45:31 -0400

    feat: theme enhancements

--- src/webui/theme/context/context-provider.tsx
+++ src/webui/theme/context/context-provider.tsx
@@ -1,13 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 
 import { ThemeContext } from './context';
 import type { Theme } from './context';
+import { useMediaQuery } from 'webui/shared';
 
 type ThemeContextProviderProps = PropsWithChildren;
 
 export const ThemeContextProvider = ({ children }: ThemeContextProviderProps) => {
-  const [theme, setTheme] = useState<Theme>('light');
+  const prefersDark = useMediaQuery('(prefers-color-scheme: dark)');
+  const [theme, setThemeState] = useState<Theme>(prefersDark ? 'dark' : 'light');
+  const THEME_STORAGE_KEY = 'theme';
+
+  const setTheme = useCallback((value: Theme) => {
+    setThemeState(value);
+    localStorage.setItem(THEME_STORAGE_KEY, value);
+  }, []);
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem(THEME_STORAGE_KEY);
+    if (storedValue) {
+      setThemeState(storedValue as Theme);
+    }
+  }, []);
 
   useEffect(() => {
     const themeCssClass = `theme-${theme}`;
