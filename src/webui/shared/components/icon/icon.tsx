2be134fd2a68e086e7fee069bbdf1a3b1f68f81d
Nadine <nadlgit@users.noreply.github.com>
2022-12-08 12:47:48 -0400

    fix: accessibility issues

--- src/webui/shared/components/icon/icon.tsx
+++ src/webui/shared/components/icon/icon.tsx
@@ -5,23 +5,24 @@ type IconProps = {
 };
 
 export const Icon = ({ imgSrc, imgAccessibleName, className }: IconProps) => {
+  const ariaHidden = !imgAccessibleName;
   const maskImage = `url(${imgSrc})`;
   const maskSize = 'cover';
-  const accessibleName = imgAccessibleName ?? '';
-  return (
-    <i
-      role="img"
-      title={accessibleName}
-      aria-label={accessibleName}
-      style={{
-        display: 'inline-block',
-        maskImage,
-        WebkitMaskImage: maskImage,
-        maskSize,
-        WebkitMaskSize: maskSize,
-        backgroundColor: 'currentColor',
-      }}
-      className={className ?? ''}
-    />
+  const commonProps = {
+    style: {
+      display: 'inline-block',
+      maskImage,
+      WebkitMaskImage: maskImage,
+      maskSize,
+      WebkitMaskSize: maskSize,
+      backgroundColor: 'currentColor',
+    },
+    className: className ?? '',
+  };
+
+  return ariaHidden ? (
+    <i aria-hidden {...commonProps} />
+  ) : (
+    <i role="img" title={imgAccessibleName} aria-label={imgAccessibleName} {...commonProps} />
   );
 };
